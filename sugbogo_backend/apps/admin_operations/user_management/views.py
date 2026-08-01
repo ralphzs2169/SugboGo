0c710237efe8ec8329dcfcd85e64e5881af895a3
Rhaz <aguilaranneth@gmail.com>
2026-08-02 00:49:37 +0800

    m1-rbac-protected-endpoints

--- sugbogo_backend/apps/admin_operations/user_management/views.py
+++ sugbogo_backend/apps/admin_operations/user_management/views.py
@@ -1,8 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from apps.authentication.permissions import HasRole
+from apps.users.models import User
 
 # Create your views here.
 @api_view(["GET"])
+@permission_classes([IsAuthenticated, HasRole(User.UserRole.ADMIN, User.UserRole.SUPER_ADMIN)])
 def get_user_management_data(request):
     return Response({
         "message": "Get User Management Data endpoint"
